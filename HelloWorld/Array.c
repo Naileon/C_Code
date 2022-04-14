8d3fccc0a25176a36061efd5cae92b65c87f5eb4
lochs <1711162985@qq.com>
2022-04-14 20:10:47 +0800

    -

---
+++ HelloWorld/Array.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+	
+int main()
+{
+	int a[10];//定义，且全部元素为随机值
+	int b[10] = { 1.2,3 };//初始化
+	int c[] = { 1,2,3 };//根据初始化数据数量，自动分配数组大小
+	int d[10] = { 0 };//可以将数组每个元素初始化为0
+	return 0;
+}
---
+++ HelloWorld/汉诺塔问题.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int f(int n) {
+	return f(n - 1) + 1;
+}
+
+int main()
+{
+	
+	return 0;
+}
