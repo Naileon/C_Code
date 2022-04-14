2474b393f557862024e8f4f9701c528978148d75
lochs <1711162985@qq.com>
2022-04-14 22:43:04 +0800

    2

--- GuessNumber/guess.c
+++ GuessNumber/guess.c
@@ -1,44 +1,23 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>//添加头文件
-#include <stdlib.h>
-
-void menu() {
-	printf("1.play\n");
-	printf("0.quit\n");
-}
-void game() {
-	int number = rand() % 10 + 1;
-	int Guess;
-	while (1) {
-		printf("请输入一个数： \n");
-		scanf("%d", &Guess);
-		if (Guess > number)
-			printf("大了");
-		else if (Guess < number)
-			printf("小了");
+#include <stdio.h>
+int main()
+{
+	int arry[] = { 1,2,3,4,5,6,7 };
+	int L = 0, R = 6; //R = 数组长度 - 1
+	int mid, target = 2;
+	while (L <= R) {
+		//这种写法最终找到时 L是等于R 的，所以不能忽略'='
+			mid = (L + R) / 2;
+		if (target < arry[mid])		//找左
+			R = mid - 1;
+		else if (target > arry[mid])//找右
+			L = mid + 1;
 		else {
-			printf("对了");
+			printf("找到%d了 位置是%d", arry[mid], mid);
 			break;
 		}
 	}
-}
-
-int main()//主函数，函数入口
-{
-	srand((unsigned int)time(NULL));
-	int input;
-	do {
-		menu();
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			return 0;
-		case 0:
-			break;
-		default:
-			printf("请重新输入\n");
-		}
-	} while (input);
+	if (L > R) {
+		printf("没找到");
+	}
+	return 0;
 }
